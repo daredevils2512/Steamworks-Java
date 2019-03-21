748ae12f1a9cd0e011f7f2a7e8ae40c42d8dd238
ike <ihschw241@isd709.org>
2019-03-21 18:14:59 -0500

    added shooter  correctly

--- Steamworks-java/src/main/java/frc/robot/Robot.java
+++ Steamworks-java/src/main/java/frc/robot/Robot.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.Drive;
 import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Shooter;
 
 /**
  * The VM is configured to automatically run this class, and to call the
